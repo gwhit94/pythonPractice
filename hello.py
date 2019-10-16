a7fe9d1b442d999f49d9d3204aa16adcae7b18e4
Graham <greysnaa@gmail.com>
2019-10-16 16:11:51 -0500

    Print lines, math basics

--- hello.py
+++ hello.py
@@ -1,3 +1,21 @@
 #! /usr/bin/python
 import sys
 sys.stdout.write("hello from Python %s\n" % (sys.version,))
+
+# class Person(object):
+#     pass
+age = 25
+height = "6'2\""
+
+floatDivision = 8 / 5 # division returns floating point variable
+floorDivision = 8 // 5
+squared = 5**2
+powers = 5**3
+
+print("Age:",age) # seperate line
+print("Height:",height) # ... prints
+print(
+"Float Division:",floatDivision,
+"| Floor Division:",floorDivision,
+"| Squared:", squared,
+"| Powers:", powers) # single line print
